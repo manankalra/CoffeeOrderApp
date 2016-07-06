66d139e97e86f8360ec741f9bf69acc816d158f1
Manan Kalra <manankalra29@gmail.com>
2016-07-07 00:19:19 +0530

    Email support

--- app/src/main/java/io/github/manankalra/justjava/MainActivity.java
+++ app/src/main/java/io/github/manankalra/justjava/MainActivity.java
@@ -1,12 +1,14 @@
 package io.github.manankalra.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -24,19 +26,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean cream, boolean chocolate) {
         int price;
-        CheckBox whippedCreamCheck = (CheckBox) findViewById(R.id.checkBox1);
-        CheckBox chocolateCheck = (CheckBox) findViewById(R.id.checkBox2);
-        if(whippedCreamCheck.isChecked() == true && chocolateCheck.isChecked() == true){
+        if(cream == true && chocolate == true){
            price = quantity * 8;
 
         }
-        else if(whippedCreamCheck.isChecked() == true && chocolateCheck.isChecked() == false){
+        else if(cream == true && chocolate == false){
             price = quantity * 6;
 
         }
-        else if(whippedCreamCheck.isChecked() == false && chocolateCheck.isChecked() == true){
+        else if(cream == false && chocolate == true){
             price = quantity * 7;
         }
         else{
@@ -46,27 +46,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * Called when the order button is clicked.
      */
+
     public void submitOrder(View view){
         if(quantity == 0){
             displayMessage("Add something to the cart!");
         }
         else{
             displayMessage(printOrderSummary());
+            EditText name = (EditText) findViewById(R.id.name);
+            String nameOfTheCustomer = name.getText().toString();
+            String subject = "Coffee order for " + nameOfTheCustomer;
+            String recipient[] = {"kalra.manan14@stu.upes.ac.in"};
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+            intent.putExtra(Intent.EXTRA_TEXT, printOrderSummary());
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
         }
     }
 
     /**
-     * This method returns the order summary
-     * @return
+     * Returns the order summary to the submitOrder method.
      */
-    private String printOrderSummary(){
+    public String printOrderSummary(){
         EditText name = (EditText) findViewById(R.id.name);
         String nameOfTheCustomer = name.getText().toString();
         CheckBox whippedCheck = (CheckBox) findViewById(R.id.checkBox1);
         CheckBox chocoCheck = (CheckBox) findViewById(R.id.checkBox2);
-        String summary = "Name: " + nameOfTheCustomer + "\nQuantity: " + quantity + "\n" + calculatePrice() + " dollars for " + quantity + " cups of coffee. Pay up!\nThank You!";
+        String summary = "Name: " + nameOfTheCustomer + "\nQuantity: " + quantity + "\n" + calculatePrice(whippedCheck.isChecked(), chocoCheck.isChecked()) + " dollars for " + quantity + " cups of coffee. Pay up!\n" + getString(R.string.thank_you);
         if(whippedCheck.isChecked() == true && chocoCheck.isChecked() == true){
             summary += "\nWe added a whipped cream and a chocolate topping, by the way! ;)";
 
@@ -78,9 +90,9 @@ public class MainActivity extends AppCompatActivity {
         else if(whippedCheck.isChecked() == false && chocoCheck.isChecked() == true){
             summary += "\nWe added a chocolate topping, by the way! ;)";
         }
-        Log.v("MainActivity", nameOfTheCustomer);
-        Log.v("MainActivity", "Add whipped cream: " + whippedCheck.isChecked());
-        Log.v("MainActivity", "Add chocolate: " + chocoCheck.isChecked());
+        //Log.v("MainActivity", nameOfTheCustomer);
+        //Log.v("MainActivity", "Add whipped cream: " + whippedCheck.isChecked());
+        //Log.v("MainActivity", "Add chocolate: " + chocoCheck.isChecked());
         return summary;
     }
 
@@ -93,7 +105,10 @@ public class MainActivity extends AppCompatActivity {
         display(quantity);
     }
     public void decrement(View view) {
-        if (quantity > 0) {
+        if (quantity == 0) {
+            Toast.makeText(this, "Just playin' around, huh?", Toast.LENGTH_SHORT).show();
+        }
+        else{
             quantity--;
             display(quantity);
         }
@@ -115,4 +130,5 @@ public class MainActivity extends AppCompatActivity {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
+
 }
